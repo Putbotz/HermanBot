1965c5b646d8db8f149330f635758e1aa4aa2336
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-01-21 13:43:00 +0800

    Update mengerror.js

--- plugins/mengerror.js
+++ plugins/mengerror.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m) => {
 let stc = fs.readFileSync('./sticker/error.webp')
-conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*ERROR BY ALAN BOTZ*', 'status@broadcast')
+conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*ERROR BY PUTBOTZ*', 'status@broadcast')
 }
 handler.customPrefix = /^(ERROR)$/i
 handler.command = new RegExp
